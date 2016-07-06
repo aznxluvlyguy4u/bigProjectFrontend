6ee3cc3567f12b015c5d0e3b50b7dff955f14ea2
D S <Dw1LS@outlook.com>
2016-07-06 17:21:48 +0200

    Implement Client View Skeleton

--- src/app/main/client/details/client.details.ts
+++ src/app/main/client/details/client.details.ts
@@ -1,8 +1,32 @@
 import {Component} from "@angular/core";
+import {Subscription} from "rxjs/Rx";
+import {Router, ActivatedRoute} from "@angular/router";
+import {TranslatePipe} from "ng2-translate/ng2-translate";
 
 @Component({
-    template: '<h1>CLIENT - DETAILS</h1>'
+    templateUrl: '/app/main/client/details/client.details.html',
+    pipes: [TranslatePipe]
 })
 
-export class ClientDetailsComponent {}
+export class ClientDetailsComponent {
+    private dataSub: Subscription;
+    private clientId: string;
+
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.dataSub = this.activatedRoute.params.subscribe(params => {
+            this.clientId = params['id'];
+        });
+    }
+
+    ngOnDestroy() {
+        this.dataSub.unsubscribe()
+    }
+
+
+    private navigateTo(url: string) {
+        this.router.navigate([url]);
+    }
+}
 
