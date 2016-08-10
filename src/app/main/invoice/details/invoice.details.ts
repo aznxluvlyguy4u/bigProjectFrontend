4b78600aa107963a8417569d48e7f010b1f8e070
D S <Dw1LS@outlook.com>
2016-08-10 13:34:35 +0200

    Add VAT Calculations

--- src/app/main/invoice/details/invoice.details.ts
+++ src/app/main/invoice/details/invoice.details.ts
@@ -20,6 +20,10 @@ export class InvoiceDetailsComponent {
     private invoiceRulesOptions: InvoiceRule[] = [];
     
     private invoice: Invoice = new Invoice;
+    
+    private totalExclVAT: number = 0;
+    private totalInclVAT: number = 0;
+    private vatCalculations = [];
 
     constructor(private router: Router, private activatedRoute: ActivatedRoute, private nsfo: NSFOService) {
         this.getInvoiceRulesOptions();
@@ -40,9 +44,6 @@ export class InvoiceDetailsComponent {
         });
     }
 
-    ngOnChange(){
-        console.log(this.selectedInvoiceId);
-    }
     private getInvoiceRulesOptions(): void {
         this.nsfo.doGetInvoiceRules()
             .subscribe(
@@ -55,10 +56,50 @@ export class InvoiceDetailsComponent {
     private addInvoiceRule(): void {
         let invoiceRule = _.find(this.invoiceRulesOptions, {'rule_id': this.selectedInvoiceId});
         this.invoice.rules.push(invoiceRule);
+        this.doVATCalculations();
     }
 
     private removeInvoiceRule(invoiceRule: InvoiceRule): void {
         let index = this.invoice.rules.indexOf(invoiceRule);
         this.invoice.rules.splice(index, 1);
+        this.doVATCalculations();
+    }
+
+    private doVATCalculations(): void {
+        this.vatCalculations = [];
+        this.totalExclVAT = 0;
+        this.totalInclVAT = 0;
+
+        let calculations = [];
+        let categories = _.uniqBy(this.invoice.rules, 'vat_rate');
+        for(let category of categories) {
+            let filteredArray = _.filter(this.invoice.rules, {'vat_rate': category.vat_rate});
+            calculations.push(filteredArray);
+        }
+
+        for(let group of calculations) {
+
+            let totalAmount = 0;
+            let vat_rate = 0;
+
+            for(let single of group) {
+                this.totalExclVAT += single.price_excl_vat;
+                totalAmount += single.price_excl_vat;
+                vat_rate = single.vat_rate;
+            }
+
+            let calculation = {
+                category: vat_rate,
+                amount: totalAmount,
+                total: (totalAmount/100) * vat_rate
+            };
+            this.totalInclVAT += (totalAmount/100) * vat_rate
+            this.vatCalculations.push(calculation);
+
+
+        }
+
+        this.totalInclVAT += this.totalExclVAT;
+        console.log(this.vatCalculations);
     }
 }
