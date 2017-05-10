7aec57f82fc3de5f0e2bf6fb6bd9697a4428f6ac
werner <werner@jongensvantechniek.nl>
2017-05-10 14:51:12 +0200

    Add a filter pipe for invoice overview, which allows for sorting base on invoice status

--- src/app/main/invoice/overview/pipes/invoiceFilter.pipe.ts
+++ src/app/main/invoice/overview/pipes/invoiceFilter.pipe.ts
@@ -1,3 +1,39 @@
-/**
- * Created by werner on 10-5-17.
- */
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+    name: 'invoiceFilter'
+})
+
+export class invoiceFilterPipe implements PipeTransform{
+    transform(list: any, args: string[]): any {
+        let statusFilter: string = args[0];
+
+        let filtered = list;
+
+        if (statusFilter == 'UNPAID INVOICES'){
+            filtered = filtered.filter(invoice => {
+                return invoice.status == 'UNPAID';
+            });
+        }
+
+        if (statusFilter == 'NOT SEND INVOICES') {
+            filtered = filtered.filter(invoice => {
+                return invoice.status == 'NOT SEND';
+            });
+        }
+
+        if (statusFilter == 'PAID INVOICES') {
+            filtered = filtered.filter(invoice => {
+                return invoice.status == 'PAID';
+            });
+        }
+
+        if (statusFilter == "INCOMPLETE INVOICES") {
+            filtered = filtered.filter(invoice => {
+                return invoice.status == 'INCOMPLETE';
+            });
+        }
+
+        return filtered;
+    }
+}
