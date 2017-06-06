8b6dc20d615d8e0ad8c1b2b4a9f9f9f1c51086b8
Peter Verkooijen <Peter@Peters-MacBook-Pro.local>
2017-06-06 10:03:03 +0200

    Modify and add uri's

--- src/app/global/services/nsfo/nsfo.service.ts
+++ src/app/global/services/nsfo/nsfo.service.ts
@@ -9,7 +9,7 @@ export class NSFOService {
     public URI_RESET_PASSWORD = '/v1/admins/auth/password-reset';
     public URI_VALIDATE_TOKEN = '/v1/auth/validate-token';
     public URI_PROVINCES = '/v1/countries/nl/provinces';
-    
+
     public URI_GHOST_LOGIN: string = '/v1/admins/ghost';
     public URI_ADMIN: string = '/v1/admins';
     public URI_ADMIN_DEACTIVATE: string = '/v1/admins-deactivate';
@@ -24,19 +24,35 @@ export class NSFOService {
 
     public URI_HEALTH_COMPANY: string = '/v1/health/company';
     public URI_HEALTH_UBN: string = '/v1/health/ubn';
-    public URI_HEALTH_INSPECTIONS: string = '/v1/health_inspections';
+    public URI_HEALTH_INSPECTIONS: string = '/v1/inspections';
     public URI_HEALTH_LOCATION_LETTERS: string = '/v1/health_location_letters';
-    
-    public URI_LIVESTOCK: string = '/v1/animals-livestock';  
+
+    public URI_LIVESTOCK: string = '/v1/animals-livestock';
     public URI_SETTINGS: string = '/v1/settings';
     public URI_LAB_RESULTS: string = '/v1/lab-results';
+
+    public URI_UBNS: string = 'v1/ubns';
+
+    public URI_LOCATIONS_TO_ANNOUNCE:string = '/v1/ubns?inspectionStatus=to_announce';
+    public URI_LOCATIONS_ANNOUNCED:string = '/v1/ubns?inspectionStatus=announced';
+    public URI_INSPECTIONS_TO_RECEIVE_LAB_RESULTS:string = '/v1/inspections?status=to_receive_lab_results';
+    public URI_INSPECTIONS_TO_AUTHORIZE:string = '/v1/inspections?status=to_authorize';
+    public URI_INSPECTIONS_FINISHED:string = '/v1/inspections?status=finished';
+    public URI_INSPECTIONS_EXPIRED:string = '/v1/inspections?status=expired';
+
+    public URI_ANNOUNCEMENTS = '/v1/announcements';
+    public URI_INSPECTIONS = '/v1/inspections';
+
+    public URI_ANNOUNCEMENTS_LETTERS = '/v1/announcements/letters';
+    public URI_INSPECTIONS_LETTERS = '/v1/inspections/letters';
+    public URI_BARCODES = '/v1/lab-results/barcodes';
 
     private content_type: string = "Content-Type";
     private authorization: string = "Authorization";
     public access_token: string = "AccessToken";
-    
+
     private ACCESS_TOKEN_NAMESPACE: string = 'access_token';
-    
+
     constructor(private http:Http) {}
 
     public doLoginRequest(username:string, password:string) {
