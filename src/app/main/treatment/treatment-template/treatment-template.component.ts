906bd0a6926836fd366219e2b6b51560bfe33a1f
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-11-09 11:46:32 +0100

    Setup Treatment components structure and routes

---
+++ src/app/main/treatment/treatment-template/treatment-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-treatment-template',
+	template: require('./treatment-template.component.html'),
+	providers: []
+})
+export class TreatmentTemplateComponent {
+
+}
