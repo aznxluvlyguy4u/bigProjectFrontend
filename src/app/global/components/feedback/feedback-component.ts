84da00bf36a02c8ff1d599a24f621fa21c2d03ce
werner <werner@jongensvantechniek.nl>
2018-05-28 10:32:14 +0200

    Implement a feedback component, that contains text that can change and shows spinners if needed

--- src/app/global/components/feedback/feedback-component.ts
+++ src/app/global/components/feedback/feedback-component.ts
@@ -1,3 +1,21 @@
-/**
- * Created by werner on 28-5-18.
- */
+import {Component, Input, OnInit} from "@angular/core";
+import {CheckMarkComponent} from "../checkmark/check-mark.component";
+import {TranslatePipe} from "ng2-translate";
+@Component({
+    selector: 'feedback-component',
+    template: require('./feedback-component.html'),
+    directives: [CheckMarkComponent],
+    pipes: [TranslatePipe]
+})
+
+export class FeedbackComponent implements OnInit {
+
+    @Input() messageText: string;
+    @Input() showSpinners: boolean;
+
+    ngOnInit() {
+
+    }
+
+
+}
