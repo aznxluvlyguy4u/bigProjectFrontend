c796228d56bc1c0612dfc53577f8301074815189
werner <werner@jongensvantechniek.nl>
2018-08-09 16:51:08 +0200

    Implement a lab result editor for maedi visna lab results

--- src/app/global/components/labresultseditor/maedivisna/maedi-visna-lab-results-editor.component.ts
+++ src/app/global/components/labresultseditor/maedivisna/maedi-visna-lab-results-editor.component.ts
@@ -1,28 +1,28 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
-import {NSFOService} from "../../services/nsfo/nsfo.service";
-import {LabResultItem, LocationHealthInspection} from "../../../main/health/health.model";
+import {NSFOService} from "../../../services/nsfo/nsfo.service";
+import {LabResultItem, LocationHealthInspection} from "../../../../main/health/health.model";
 import {Headers, Http, RequestOptions} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {TranslatePipe} from "ng2-translate";
 import {FormBuilder, FormGroup, REACTIVE_FORM_DIRECTIVES, Validator, Validators} from "@angular/forms";
-import {LabResultFile} from "../../models/lab-result-file.model";
-import {LabResult, LabResult} from "../../models/lab-result.model";
+import {LabResultFile} from "../../../models/lab-result-file.model";
+import {LabResultMaediVisna} from "../../../models/lab-result.model";
 
 @Component({
-    selector: 'lab-results-editor',
-    template: require('./lab-results-editor.component.html'),
+    selector: 'maedi-visna-lab-results-editor',
+    template: require('./maedi-visna-lab-results-editor.component.html'),
     pipes: [TranslatePipe],
     directives: [REACTIVE_FORM_DIRECTIVES]
 })
 
-export class LabResultsEditorComponent {
+export class MaediVisnaLabResultsEditorComponent {
     @Input() inspection: LocationHealthInspection;
-    @Input() labResults: LabResult[];
+    @Input() labResults: LabResultMaediVisna[];
     @Output() revisedResults = new EventEmitter<LabResultFile[]>();
     private displayModal = 'none';
 
     constructor(private apiService: NSFOService, private fb: FormBuilder){
-        
+
     }
 
     openModal() {
