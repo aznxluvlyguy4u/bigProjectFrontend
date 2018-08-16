563bde063c9d95f937384e536c29580bab5891c8
werner <werner@jongensvantechniek.nl>
2018-08-16 16:09:03 +0200

    Add gulpfile comments?

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,23 @@ var staging_admin_frontend_bucket = 'dev-admin.nsfo.nl';
 var production_admin_frontend_bucket = 'admin.nsfo.nl';
 var public_read = 'public-read';
 var retry_count = 5;
+/**
+ * Load environment variables from JSON File.
+ */
 env({
     file: 'env.json'
 });
+/**
+ * Initialize AWS Config
+ */
 var awsConfig = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     useIAM: true
 };
+/**
+ * Task to zip the build directory
+ */
 gulp.task('zip:staging', function () {
     return gulp
         .src(['build/**/*'], { dot: true })
@@ -32,6 +41,9 @@ gulp.task('zip:prod', function () {
         .pipe(zip(filename + '-prod-' + version + '.zip'))
         .pipe(gulp.dest('dist'));
 });
+/**
+ * Task to deploy build files directly to AWS S3 hosted website Bucket.
+ */
 gulp.task('publish:staging', function () {
     return gulp.src(['build/**/*'])
         .pipe(s3({
@@ -50,6 +62,9 @@ gulp.task('publish:prod', function () {
         maxRetries: retry_count
     }));
 });
+/**
+ * Task to deploy maintenance page directly to AWS S3 hosted website Bucket.
+ */
 gulp.task('publish:staging:maintenance', function () {
     return gulp.src(['src/maintenance-page/*'])
         .pipe(s3({
@@ -68,6 +83,9 @@ gulp.task('publish:prod:maintenance', function () {
         maxRetries: retry_count
     }));
 });
+/**
+ * Task to deploy the zip file to AWS S3 Bucket
+ */
 gulp.task('publish:zip:staging', function () {
     var zipname = filename + '-staging-';
     return gulp.src('dist/' + zipname + version + '.zip')
@@ -92,4 +110,3 @@ gulp.task('clean-scripts', function () {
     return gulp.src(['src/app/**/*.js', 'src/app/**/*.js.map'], { read: false, force: true })
         .pipe(clean());
 });
-//# sourceMappingURL=gulpfile.js.map
