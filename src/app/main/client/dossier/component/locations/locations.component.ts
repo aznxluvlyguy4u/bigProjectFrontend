ee41376ffd9cac30557daf79459a31a0fe6a8407
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-09-13 15:48:31 +0200

    Fix locations.component for client dossier

--- src/app/main/client/dossier/component/locations/locations.component.ts
+++ src/app/main/client/dossier/component/locations/locations.component.ts
@@ -1,6 +1,6 @@
 import _ = require("lodash");
 import {Component, Input, Output} from "@angular/core";
-import {REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder, FormControl} from "@angular/forms";
+import {REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder} from "@angular/forms";
 import {TranslatePipe} from "ng2-translate/ng2-translate";
 import {Location} from "../../../client.model";
 import {UtilsService} from "../../../../../global/services/utils/utils.service";
@@ -20,6 +20,8 @@ import { CountryNameToCountryCodePipe } from '../../../../../global/pipes/countr
 })
 
 export class LocationsDisplay {
+    public objectKeys = Object.keys;
+
     private provinces = [];
     private provinces$: Subscription;
     private displayModal: string = 'none';
@@ -28,7 +30,7 @@ export class LocationsDisplay {
     private isValidForm: boolean = true;
     private form: FormGroup;
     private location: Location = new Location();
-    private locationTemp: Location;
+    private selectedLocationKey: any;
 
     @Input() locations: Location[] = [];
     @Input() disabledMode: boolean = false;
@@ -76,14 +78,17 @@ export class LocationsDisplay {
         }
     }
     
-    private openModal(editMode: boolean, location: Location): void {
+    public openModal(editMode: boolean, selectedLocationKey?: any): void {
         this.isModalEditMode = editMode;
         this.displayModal = 'block';
+        this.selectedLocationKey = selectedLocationKey;
+    }
 
-        if(editMode) {
-            this.location = _.cloneDeep(location);
-            this.locationTemp = _.cloneDeep(location);
+    public getModalLocation(): Location {
+        if (this.isModalEditMode) {
+            return this.locations[this.selectedLocationKey];
         }
+        return this.location;
     }
 
     private closeModal(): void {
@@ -93,12 +98,11 @@ export class LocationsDisplay {
         this.resetValidation();
     }
     
-    private addLocation(): void {
+    public addLocation(): void {
         this.isValidForm = true;
 
         if(this.form.valid && this.isValidForm) {
             let isUniqueUBN = this.checkForUniqueUBN(this.location);
-
             if(isUniqueUBN) {
                 this.locations.push(this.location);
                 this.getLocations.emit(this.locations);
@@ -111,23 +115,23 @@ export class LocationsDisplay {
         }
     }
 
-    private removeLocation(location: Location): void {
-        _.remove(this.locations, location);
+    public getLocationByKey(key: any): Location {
+        return this.locations[key];
+    }
+
+    public removeLocation(key: any): void {
+        const locationToDelete = this.getLocationByKey(key);
+        _.remove(this.locations, locationToDelete);
         this.removeProvinceIfCountryIsNotNetherlands();
         this.getLocations.emit(this.locations);
-    }
-
-    private sendDeletedLocation(location: Location) {
-        this.getDeletedLocations.emit(location);
+			  this.getDeletedLocations.emit(locationToDelete);
     }
     
-    private editLocation(): void {
+    public editLocation(): void {
         if(this.form.valid && this.isValidForm) {
             let isUniqueUBN = this.checkForUniqueUBN(this.location);
-
+					console.log(isUniqueUBN);
             if(isUniqueUBN) {
-                this.removeLocation(this.locationTemp);
-                this.locations.push(this.location);
                 this.getLocations.emit(this.locations);
                 this.closeModal();
             } else {
@@ -140,11 +144,6 @@ export class LocationsDisplay {
 
     private checkForUniqueUBN(location: Location) {
         let index = _.findIndex(this.locations, ['ubn', location.ubn]);
-
-        if (this.locationTemp) {
-            return index < 0 || this.locationTemp.ubn == this.location.ubn;
-        }
-
         return index < 0;
     }
 
